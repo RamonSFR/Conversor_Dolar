a48684c4fcfc789e19f1e99771df0abe22294c19
RamonSFR <ramonsaviofontesrocha@gmail.com>
2023-09-19 16:04:53 -0300

    .

--- scripts.js
+++ scripts.js
@@ -2,17 +2,15 @@ const reais = document.getElementById("reais");
 const dolares = document.getElementById("dolares");
 
 reais.addEventListener('input', () => {
-    const valorReais = parseFloat(reais.value);
-    const taxaConversao = valorReais * 4.85; // Para o dia 18/09/2023 15:23 BRT
+    const valorReais = parseFloat(reais.value)
+    const conversao = valorReais * 0.21;
 
-    const valorDolares = taxaConversao;
-    dolares.value = valorDolares.toFixed(2);
+    dolares.value = conversao.toFixed(2);
 })
 
 dolares.addEventListener('input', () => {
-    const valorDolares = parseFloat(reais.value);
-    const taxaConversao = valorDolares * 0.21; // Para o dia 18/09/2023 15:23 BRT
+    const valorDolares = parseFloat(dolares.value)
+    const conversao = valorDolares * 4.86;
 
-    const valorReais = taxaConversao;
-    reais.value = valorReais.toFixed(2);
+    reais.value = conversao.toFixed(2);
 })
